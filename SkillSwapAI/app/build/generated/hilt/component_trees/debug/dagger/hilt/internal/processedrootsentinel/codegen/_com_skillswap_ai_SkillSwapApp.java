57f50014d7ad181727882a9c5368c4ed7a32b3c1
Ajay <ajay@example.com>
2026-08-12 13:21:17 +0530

    Implement Jitsi session completion and post-meeting rating flow

--- SkillSwapAI/app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_skillswap_ai_SkillSwapApp.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.skillswap.ai.SkillSwapApp"
-)
-public final class _com_skillswap_ai_SkillSwapApp {
-}
